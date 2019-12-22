723024a716e545bcb5e85fb1b8d327e83ecdbab8
ArcSoftEscErd <ArcSoftEscErd>
2019-12-22 15:15:30 +0800

    【添加注释】

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -61,19 +61,36 @@ JNICALL
 Java_com_wsy_jnidemo_MainActivity_testCallJava(
         JNIEnv *env,
         jobject /* this */, jobject activity) {
+    // 第二个参数this被省略了，实际上，这个参数也是activity，但是在这里我们手动传入演示
+
+    // 首先获取MainActivity的类
     jclass cls = env->GetObjectClass(activity);
+    // 获取这个类中的code成员变量ID
     jfieldID codeId = env->GetFieldID(cls, "code", "I");
+    // 获取这个类中的msg成员变量ID
     jfieldID msgId = env->GetFieldID(cls, "msg", "Ljava/lang/String;");
+
+    // 获取code成员变量的值
     jint code = env->GetIntField(activity, codeId);
 
-    //获取MainActivity中定义的code和msg，并以log打印
+    // 获取msg成员变量的值
     jstring msg = (jstring) env->GetObjectField(activity, msgId);
-    LOGI("code = %d,msg = %s", code, env->GetStringUTFChars(msg, JNI_FALSE));
+
+    // 获取java.lang.String对象中的内容
+    const char *cMsg = env->GetStringUTFChars(msg, JNI_FALSE);
+    // 打印日志
+    LOGI("code = %d,msg = %s", code, cMsg);
+    // 用完String后要释放
+    env->ReleaseStringUTFChars(msg, cMsg);
+
+    // 找到MainActivity类中的cCallJava函数
     jmethodID callJavaMethodId = env->GetMethodID(cls, "cCallJava", "(Ljava/lang/String;)V");
+    // 创建一个java.lang.String对象，内容如下
     jstring nativeMsg = env->NewStringUTF("java method cCallJava called");
-    //调用java中的cCallJava方法
+    // 调用java中的cCallJava方法
     env->CallVoidMethod(activity, callJavaMethodId, nativeMsg);
 
+    // 这里的DeleteLocalRef可以不执行，在函数执行完毕后会自动释放，但是在循环次数较多的循环中需要Delete，否则可能会溢出
     env->DeleteLocalRef(msg);
     env->DeleteLocalRef(nativeMsg);
     env->DeleteLocalRef(cls);
